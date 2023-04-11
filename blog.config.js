f805ca968f9e9ebb68d4caa71a5c0b3bf03b8a1e
anyoneyo <100776099+anyoneyo@users.noreply.github.com>
2023-04-11 21:27:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,6 +2,8 @@ const BLOG = {
   title: 'NaN',
   author: 'NaN',
   description: '一些记录',
+  email: '',
+  link: '',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -16,6 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  socialLink: 'https://twitter.com/craigaryhart',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
